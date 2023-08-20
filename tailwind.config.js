d63e661c827be0c812e4386e9961f5cebdafcb22
Macko420 <maciekkoop@gmail.com>
2023-08-20 22:34:54 +0200

    deploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,16 @@ module.exports = {
       colors: {
         pgreen: "#1B5B31",
       },
+      keyframes: {
+        "slide-in": {
+          "0%": { transform: "translateX(-100%)", opacity: "0" },
+          "50%": { transform: "translateX(-50%)", opacity: "0" },
+          "100%": { transform: "translateX(0)", opacity: "1" },
+        },
+      },
+      animation: {
+        "slide-in": "slide-in 1s ease-out",
+      },
     },
   },
   plugins: [],
